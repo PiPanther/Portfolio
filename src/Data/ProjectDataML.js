e7c6fa48c39ee7f35448312886493fcd3f43fa8b
A   S   H   I   S   H <96686068+PiPanther@users.noreply.github.com>
2024-08-20 04:36:29 +0530

    Improvements

--- src/Data/ProjectDataML.js
+++ src/Data/ProjectDataML.js
@@ -1,10 +1,6 @@
-import cryptzy from '../assets/cryptzzy.png'
-import JapanPrediction  from '../assets/JapanPrediction .png'
-import notes from "../assets/notes_app.png"
-import Hoomyz from '../assets/Hoomyz.png'
-import Twitzy from '../assets/Twitzy.png'
-import TextEditzy from "../assets/TextEditor.png"
 
+import yt from '../assets/yt.png'
+import tcc from '../assets/tcc.png'
 
 
 
@@ -14,14 +10,14 @@ import TextEditzy from "../assets/TextEditor.png"
             desc: "The Toxic Comment Classifier is a machine learning project designed to identify and classify toxic comments in online discussions. This tool aims to improve online communication by detecting and flagging toxic content such as insults, threats, and hate speech ",
             github_link : "https://github.com/PiPanther/ToxicCommentClassifier",
             
-            img : Hoomyz,
+            img : tcc,
         },
         {
             name: "AI Assisted Yoga Trainer",
             desc: "Developed an AI-assisted yoga trainer app utilizing MoveNet and PoseNet for real-time pose detection and analysis. The app provides personalized yoga guidance by evaluating users' poses, offering corrections, and tracking progress.",
             github_link : "https://github.com/PiPanther/AI-Assisted-Yoga-Trainer",
             
-            img : Hoomyz,
+            img : yt,
         },
         
 ]
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ export const Hero = () => {
     const [loaded, setLoaded] = useState(true);
   return (
     <>
-     {loaded ?
+     {false ?
         <div
             className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white flex flex-col items-center justify-center"
         >Loading...</div>
